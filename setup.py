40311e16ff23ecd53c12b8d419fbcd2861e3c632
i0ntempest <szf1234@me.com>
2020-04-12 18:14:35 -0400

    Preparation continues

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author       = 'i0ntempest',
     author_email = 'szf1234@me.com',
     url          = 'https://github.com/i0ntempest/powerline-exitstatus-kai',
-    download_url = 'https://github.com/i0ntempest/powerline-exitstatus-kai/archive/v' + version + '.tar.gz',
+    download_url = 'https://github.com/i0ntempest/powerline-exitstatus-kai/archive/v1.2.0.tar.gz',
     packages     = ['powerline_exitstatus_kai'],
     install_requires = ['powerline-status'],
     classifiers  = [
